1dbdf98bf1c08850e224c571ceda63daaa4f7b79
Angel Motta <angelmotta@gmail.com>
2022-08-02 20:19:21 -0500

    feat: setHead work in progress

--- python/DoubleLinkedList/main.py
+++ python/DoubleLinkedList/main.py
@@ -11,7 +11,18 @@ class DoublyLinkedList:
         self.tail = None
 
     def setHead(self, node):
-        pass
+        # Base case
+        if self.head == None:
+            self.head = node
+            self.tail = node
+            return
+        # Case: stand-alone node
+        if (node.next == None and node.prev == None):
+            initialHead = self.head
+            node.next = initialHead
+            self.head = node
+            initialHead.prev = node
+        # TODO: Case existing node
 
     def setTail(self, node):
         pass
@@ -34,7 +45,32 @@ class DoublyLinkedList:
     def containsNodeWithValue(self, value):
         pass
 
+    def traverseForward(self):
+        node = self.head
+        while(node != None):
+            print(node.value)
+            node = node.next
+        print("")
+
+    def traverseBackward(self):
+        node = self.tail
+        while(node != None):
+            print(node.value)
+            node = node.prev
+        print("")
+
 
 # Start execution
 if __name__ == '__main__':
     mylist = DoublyLinkedList()
+
+    node3 = Node(3)
+    node2 = Node(2)
+    node1 = Node(1)
+
+    mylist.setHead(node3)
+    mylist.setHead(node2)
+    mylist.setHead(node1)
+
+    mylist.traverseForward()
+    mylist.traverseBackward()

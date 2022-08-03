47001d0a0c1da6afc59154d7781ca65334edeb2e
Angel Motta <angelmotta@gmail.com>
2022-08-02 19:57:33 -0500

    feat: DoubleLinkedList implementation

---
+++ python/DoubleLinkedList/main.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        pass
+
+    def setTail(self, node):
+        pass
+
+    def insertBefore(self, node, nodeToInsert):
+        pass
+
+    def insertAfter(self, node, nodeToInsert):
+        pass
+
+    def insertAtPosition(self, position, nodeToInsert):
+        pass
+
+    def removeNodesWithValue(self, value):
+        pass
+
+    def remove(self, node):
+        pass
+
+    def containsNodeWithValue(self, value):
+        pass
+
+
+# Start execution
+if __name__ == '__main__':
+    mylist = DoublyLinkedList()
